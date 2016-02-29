25ca94dac0159194a92c23476a68209aae248126
Nikolas Haag <nikolashaag@gmail.com>
2016-02-29 22:17:54 +0100

    landing complete

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,13 +9,36 @@ var SideBar = React.createClass({
         router: React.PropTypes.func
     },
 
+    liftLanding: function () {
+        console.log('aaa');
+        console.log(document.getElementsByClassName("side-bar"));
+        var orig = document.getElementsByClassName("side-bar")[0].className;
+        document.getElementsByClassName("side-bar")[0].className = orig + ' visible';
+        orig = document.getElementsByClassName("content")[0].className;
+        document.getElementsByClassName("content")[0].className = orig + ' visible';
+        document.getElementById("full-page").className = "lifted";
+        document.getElementById("big-card").className = "lifted";
+        document.getElementById("blog-wrap").className = "fade-away";
+    },
+
     render : function() {
 
         return (
-            <section className="full-page">
-                <h1>Hello! This is some random text.</h1>
-
-                <div className="arrow">A</div>
+            <section id="full-page">
+                <div id="big-card">
+                    <h1>Nikolas Haag / dead pixel</h1>
+                    <div className="icon-wrapper">
+                        <i className="social"></i>
+                        <i className="social"></i>
+                        <i className="social"></i>
+                        <i className="social"></i>
+                        <i className="social"></i>
+                    </div>
+                </div>
+                <div id="blog-wrap" onClick={this.liftLanding}>
+                    <h2>Blog</h2>
+                    <i className="arrow"></i>
+                </div>
             </section>
         )
     }
